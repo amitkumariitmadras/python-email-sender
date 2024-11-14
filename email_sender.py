441460419c7af9fd559fed6cc1a709d78d70f2ed
amitkumariitm <amitkumarsumit62027@gmail.com>
2024-11-15 02:41:54 +0530

    email sender using smtplib working in html format

--- email_sender.py
+++ email_sender.py
@@ -9,7 +9,7 @@ def send_email(subject, message, sender_email, sender_password, recipient_email,
         server.starttls()
         print("Sending email: ", sender_email, "Sender Password: ", sender_password)
         server.login(sender_email, sender_password)
-        msg = MIMEText(message)
+        msg = MIMEText(message, 'html')
         msg['Subject'] = subject
         msg['From'] = sender_email
         msg['To'] = recipient_email
@@ -21,9 +21,28 @@ def send_email(subject, message, sender_email, sender_password, recipient_email,
 
 if __name__ == '__main__':
     recipient_email = input("Please enter the recipient's email address: ")
-    subject = input("Please enter the subject of the email: ")
-    message = input("Please enter the message: ")
+    recipient_name = input("Please enter the name of recipient: ")
+
+    subject = 'Happy Birthday!'
+
+    message_template = """
+    <html>
+        <body>
+            <h1 style="color: #2E8B57;">Happy Birthday, {name}!</h1>
+            <p>Dear <b>{name}</b>,</p>
+            <p>Wishing you a day filled with love, laughter, and joy. Here's to another year of fantastic adventures!</p>
+            <p>Enjoy your special day to the fullest!</p>
+            <br>
+            <p>Best wishes,</p>
+            <p><i>Your Friend</i></p>
+        </body>
+    </html>
+    """
+
+    message = message_template.format(name=recipient_name)
     send_email(subject, message, sender_email, sender_password, recipient_email, smtp_server, smtp_port)
+
+
 # yatharthsingh1504@gmail.com
 # hey how are you doing
 # bro this is email from business account
